c68b7ec810e996fefe27b7e8dfdd5f8946ddb205
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2021-01-09 13:01:58 +0000

    Created e.target for policyAreasClick function
    - Added e.target.classList.add("actvie") to policyAreas function
    - removed individual (Add active to policy) lines of code for each event listener

--- js/policies-and-guidance-script.js
+++ js/policies-and-guidance-script.js
@@ -47,39 +47,33 @@ const resilienceContainer = document.getElementById("resilience-button-container
 // TOP HORIZONTAL POLICY LIST EVENT LISTENERS
 // ================================================================================
 
-governanceRiskAndComplianceButton.addEventListener("click", () => {
-  hideButtonsAndRemoveActive()
-  governanceRiskAndComplianceButton.classList.add("active")
+governanceRiskAndComplianceButton.addEventListener("click", (e) => {
+  policyAreasClick(e)
   governanceRiskAndComplianceContainer.style.display = "block"
 })
 
-informationAssetManagementButton.addEventListener("click", () => {
-  hideButtonsAndRemoveActive()
-  informationAssetManagementButton.classList.add("active")
+informationAssetManagementButton.addEventListener("click", (e) => {
+  policyAreasClick(e)
   informationAssetManagementContainer.style.display = "block"
 })
 
-informationTechnologyButton.addEventListener("click", () => {
-  hideButtonsAndRemoveActive()
-  informationTechnologyButton.classList.add("active")
+informationTechnologyButton.addEventListener("click", (e) => {
+  policyAreasClick(e)
   informationTechnologyContainer.style.display = "block"
 })
 
-peopleButton.addEventListener("click", () => {
-  hideButtonsAndRemoveActive()
-  peopleButton.classList.add("active")
+peopleButton.addEventListener("click", (e) => {
+  policyAreasClick(e)
   peopleContainer.style.display = "block"
 })
 
-physicalAndEnvironmentalButton.addEventListener("click", () => {
-  hideButtonsAndRemoveActive()
-  physicalAndEnvironmentalButton.classList.add("active")
+physicalAndEnvironmentalButton.addEventListener("click", (e) => {
+  policyAreasClick(e)
   physicalAndEnvironmentalContainer.style.display = "block"
 })
 
-resilienceButton.addEventListener("click", () => {
-  hideButtonsAndRemoveActive()
-  resilienceButton.classList.add("active")
+resilienceButton.addEventListener("click", (e) => {
+  policyAreasClick(e)
   resilienceContainer.style.display = "block"
 })
 
@@ -87,14 +81,14 @@ resilienceButton.addEventListener("click", () => {
 // FUNCTIONS
 // ================================================================================
 
-// FUNCTION TO HIDE ALL BUTTON CONTAINER ELEMENTS ON LEFT HAND SIDE
-// AND TO REMOVE ACTIVE CLASS ON TOP BUTTONS
+// FUNCTION FOR HORIZONTAL POLICY CATEOGRY AREA BUTTON CLICK
 
-function hideButtonsAndRemoveActive() {
+function policyAreasClick(e) {
   for (let i = 0; i < buttonContainerArray.length; i++ ){
     buttonContainerArray[i].style.display = "none"
   }
   for (let i = 0; i < policyAreasContainerButtons.length; i++) {
     policyAreasContainerButtons[i].classList.remove("active")
   }
+    e.target.classList.add("active")
 }
